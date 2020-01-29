b04e9b21955c2a5070b978501672ab10d67d5074
A06438DIRN_P2C.04.05 <A06438DIRN_P2C.04.05@P2C-9511NRB.dir.svc.accenture.com>
2020-01-29 15:10:11 +0530

    changes in the runner class of 29th of jan

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"},tags="@data-driven")
+@CucumberOptions(plugin = {"json:target/cucumber.json","html:target/html/html-report"})
 public class RunCukesTest 
 {
 	
