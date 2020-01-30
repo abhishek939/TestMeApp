98a6ddd7ef2dcfcf98dcc274d38ef02387334e78
A06438DIRN_P2C.04.05 <A06438DIRN_P2C.04.05@P2C-9511NRB.dir.svc.accenture.com>
2020-01-30 16:12:22 +0530

    nivceday

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 /*@CucumberOptions(plugin = {"json:target/cucumber.json","html:target/html/html-report"})*/
-@CucumberOptions(plugin = {"pretty"},tags= {"not @registration","not @data-driven", "@search"})
+@CucumberOptions(plugin = {"pretty"},tags= {"not @registration","not @data-driven", "@search","@tag1"})
 public class RunCukesTest 
 {
 	
